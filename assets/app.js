028450f6f5d6c4e6e7f0ec8965b82b1448d7e1a4
Ken <khuynh024@gmail.com>
2018-07-02 18:58:47 -0500

    finished

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,16 @@ $(document).ready(function () {
     $('.modal').modal();
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
+     
 
     $('.materialboxed').materialbox();
 
+    $('.repeat').click(function(){
+        var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){ 
+            $(indicator).parent().addClass(classes);
+        }, 20);
+    });
 });
